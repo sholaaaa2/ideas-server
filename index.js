c5a90ecd83542b358adb6d844d482e24ddd2982d
FrontoMan2 <frontoman3@gmail.com>
2026-09-24 19:02:23 +0300

    deploy-debug-v7

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 import App from './app.js';
 import { env } from './src/config/env.js';
 
+console.log("===== DEPLOY TEST 24-09-2026 V7 =====");
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("Mongo URI exists:", !!process.env.MONGO_URI);
+
 mongoose
     .connect(env.mongo.url)
     .then(() => {
